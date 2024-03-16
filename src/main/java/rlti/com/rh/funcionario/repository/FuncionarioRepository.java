11b12e6108ce4079c6b5a2cc6d18260e00c699b2
ranelho <ranelhol@gmail.com>
2024-03-16 11:25:15 -0300

    implementa save funcionario

---
+++ src/main/java/rlti/com/rh/funcionario/repository/FuncionarioRepository.java
@@ -0,0 +1,7 @@
+package rlti.com.rh.funcionario.repository;
+
+import rlti.com.rh.funcionario.domain.Funcionario;
+
+public interface FuncionarioRepository {
+    boolean save(Funcionario funcionario);
+}
