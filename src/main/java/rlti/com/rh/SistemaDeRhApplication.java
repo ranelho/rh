244c660f52c80871f8bba3c3dd2848fa63b63e4d
ranelho <ranelhol@gmail.com>
2024-03-16 10:16:54 -0300

    implementa jpa auditing

--- src/main/java/rlti/com/rh/SistemaDeRhApplication.java
+++ src/main/java/rlti/com/rh/SistemaDeRhApplication.java
@@ -2,8 +2,10 @@ package rlti.com.rh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SistemaDeRhApplication {
 
 	public static void main(String[] args) {
