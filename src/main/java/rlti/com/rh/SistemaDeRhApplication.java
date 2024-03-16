3b9dbc52901ac8b9c0f4b6e25fa1eff423d194a2
ranelho <ranelhol@gmail.com>
2024-03-16 09:26:28 -0300

    configura projeto

---
+++ src/main/java/rlti/com/rh/SistemaDeRhApplication.java
@@ -0,0 +1,13 @@
+package rlti.com.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeRhApplication.class, args);
+	}
+
+}
