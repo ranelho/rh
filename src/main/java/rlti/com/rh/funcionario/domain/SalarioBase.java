e47bf53a019be50c61101059de0a08b44a5e9178
ranelho <ranelhol@gmail.com>
2024-03-19 14:39:06 -0300

    restruturação

--- src/main/java/rlti/com/rh/funcionario/domain/SalarioBase.java
+++ src/main/java/rlti/com/rh/funcionario/domain/SalarioBase.java
@@ -23,4 +23,5 @@ public class SalarioBase {
     private Double valorSalario;
     @CreatedDate
     LocalDateTime createdAt;
+    private Long nivel;
 }
--- src/main/java/rlti/com/rh/funcionario/domain/SituacaoCurso.java
+++ src/main/java/rlti/com/rh/funcionario/domain/SituacaoCurso.java
@@ -3,5 +3,6 @@ package rlti.com.rh.funcionario.domain;
 public enum SituacaoCurso {
     CONCLUIDO,
     EM_ANDAMENTO,
+    TRANCADO,
     CANCELADO;
 }
