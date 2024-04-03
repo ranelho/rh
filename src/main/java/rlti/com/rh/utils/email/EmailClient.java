6c8c0199f903765efb6665f104031f3508e948e6
ranelho <ranelhol@gmail.com>
2024-04-03 16:00:46 -0300

    ajustes bath

--- src/main/java/rlti/com/rh/utils/email/EmailClient.java
+++ src/main/java/rlti/com/rh/utils/email/EmailClient.java
@@ -2,8 +2,6 @@ package rlti.com.rh.utils.email;
 
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
