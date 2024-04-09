9f752c1db1b5e7f821f9e9d8baa63376221278ef
ranelho <ranelhol@gmail.com>
2024-04-09 19:12:48 -0300

    implementa codigos

--- src/main/java/com/rlti/rh/horas/infra/CodigoJpaRepository.java
+++ src/main/java/com/rlti/rh/horas/infra/CodigoJpaRepository.java
@@ -3,6 +3,9 @@ package com.rlti.rh.horas.infra;
 import com.rlti.rh.codigos.domain.Codigo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CodigoJpaRepository extends JpaRepository<Codigo, Long>{
     Codigo findByCod(String codigo);
+    Optional<Codigo> findByCodOrDescricao(String cod, String descricao);
 }
--- src/main/java/com/rlti/rh/horas/repository/CodigoRepository.java
+++ src/main/java/com/rlti/rh/horas/repository/CodigoRepository.java
@@ -2,6 +2,12 @@ package com.rlti.rh.horas.repository;
 
 import com.rlti.rh.codigos.domain.Codigo;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CodigoRepository {
     Codigo findByCodigo(String codigo);
+    Codigo save(Codigo codigo);
+    List<Codigo> findAll();
+    Optional<Codigo> findByCodigoOrDescricao(String cod, String descricao);
 }
